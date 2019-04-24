008c36ae08edb6a69ee2bf875c4d5cdcc0f4c223
elane23 <152497434@qq.com>
2019-04-24 01:52:11 -0400

    setup

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
 }
+
+
 
